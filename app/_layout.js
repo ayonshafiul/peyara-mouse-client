05059e543665570a34018613ace2bacd2a3e3658
ayonshafiul <ayonshafiul@gmail.com>
2023-09-06 04:43:11 +0600

    feat: add experimental touchpad

--- app/_layout.js
+++ app/_layout.js
@@ -1,4 +1,9 @@
 import { Slot } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function App() {
-  return <Slot />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Slot />
+    </GestureHandlerRootView>
+  );
 }
--- app/index.js
+++ app/index.js
@@ -1,20 +1,57 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useRef } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  PanGestureHandler,
+  TapGestureHandler,
+} from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  runOnJS,
+} from "react-native-reanimated";
 import { io } from "socket.io-client";
 const socket = io.connect("http://192.168.0.101:1313");
 export default function App() {
-  const sendMessage = () => {
-    socket.emit("coordinates", { hello: "world" });
+  const doubleTapRef = useRef();
+  const sendMessage = (data) => {
+    socket.emit("coordinates", data);
+  };
+  const touchPadGestureHandler = useAnimatedGestureHandler({
+    onStart: (event) => {
+      console.log(event.translationX, event.velocityX);
+    },
+    onActive: (event) => {
+      let coordinates = {
+        x: event.translationX,
+        y: event.translationY,
+        vx: event.velocityX,
+        vy: event.velocityY,
+      };
+
+      runOnJS(sendMessage)(coordinates);
+    },
+    onEnd: (event) => {},
+  });
+  const handleSingleMouseTap = (event) => {
+    console.log(event);
   };
   return (
     <View style={styles.container}>
-      <Text>Index</Text>
-      <Link href={"/qrcode"}>Go To QrCode</Link>
-      <Pressable onPress={sendMessage}>
-        <Text>Send</Text>
-      </Pressable>
-      <StatusBar style="auto" />
+      <PanGestureHandler onGestureEvent={touchPadGestureHandler}>
+        <Animated.View style={styles.touchpad}></Animated.View>
+      </PanGestureHandler>
+
+      {/* <View style={styles.buttonContainer}>
+        <TapGestureHandler
+          onHandlerStateChange={handleSingleMouseTap}
+          waitFor={doubleTapRef}
+        >
+          <TapGestureHandler ref={doubleTapRef} numberOfTaps={2}>
+            <View style={styles.button}></View>
+          </TapGestureHandler>
+        </TapGestureHandler>
+      </View> */}
     </View>
   );
 }
@@ -25,5 +62,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  touchpad: {
+    width: "100%",
+    height: 200,
+    margin: 16,
+    borderRadius: 8,
+    backgroundColor: "green",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 10,
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "blue",
   },
 });
