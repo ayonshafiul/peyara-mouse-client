a22abb2a33676a6aa8124fe702095d5e2b723133
ayonshafiul <ayonshafiul@gmail.com>
2023-09-07 07:33:10 +0600

    feat: add full mouse support with drag gestures

--- app/index.js
+++ app/index.js
@@ -2,45 +2,110 @@ import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useRef } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
-import {
-  PanGestureHandler,
-  TapGestureHandler,
-} from "react-native-gesture-handler";
-import Animated, {
-  useAnimatedGestureHandler,
-  runOnJS,
-} from "react-native-reanimated";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, { runOnJS } from "react-native-reanimated";
 import { io } from "socket.io-client";
 const socket = io.connect("http://192.168.0.101:1313");
+
 export default function App() {
   const doubleTapRef = useRef();
-  const sendMessage = (data) => {
-    socket.emit("coordinates", data);
+  const sendCoordinates = (coordinates) => {
+    socket.emit("coordinates", coordinates);
   };
-  const touchPadGestureHandler = useAnimatedGestureHandler({
-    onStart: (event) => {
-      console.log(event.translationX, event.velocityX);
-    },
-    onActive: (event) => {
-      let coordinates = {
-        x: event.translationX,
-        y: event.translationY,
-        vx: event.velocityX,
-        vy: event.velocityY,
-      };
+  const sendScroll = (coordinates) => {
+    socket.emit("scroll", coordinates);
+  };
+  const sendClicks = (state) => {
+    socket.emit("clicks", state);
+  };
 
-      runOnJS(sendMessage)(coordinates);
-    },
-    onEnd: (event) => {},
-  });
-  const handleSingleMouseTap = (event) => {
-    console.log(event);
+  const sendWindowDragStart = (coordinates) => {
+    socket.emit("windowdragstart", coordinates);
   };
+  const sendWindowDragUpdate = (coordinates) => {
+    socket.emit("windowdragupdate", coordinates);
+  };
+  const sendWindowDragEnd = (coordinates) => {
+    socket.emit("windowdragend", coordinates);
+  };
+
+  const dragGesture = Gesture.Pan()
+    .onStart((_e) => {})
+    .onUpdate((e) => {
+      let coordinates = {
+        x: e.translationX,
+        y: e.translationY,
+      };
+      runOnJS(sendCoordinates)(coordinates);
+    })
+    .onEnd(() => {});
+  const dragGestureScroll = Gesture.Pan()
+    .minPointers(2)
+    .onStart((_e) => {})
+    .onUpdate((e) => {
+      let coordinates = {
+        x: e.translationX,
+        y: e.translationY,
+      };
+      runOnJS(sendScroll)(coordinates);
+    })
+    .onEnd(() => {});
+  const dragGestureWindowDrag = Gesture.Pan()
+    .minPointers(3)
+    .onStart((e) => {
+      runOnJS(sendWindowDragStart)();
+    })
+    .onUpdate((e) => {
+      let coordinates = {
+        x: e.translationX,
+        y: e.translationY,
+      };
+      runOnJS(sendWindowDragUpdate)(coordinates);
+    })
+    .onEnd((e) => {
+      runOnJS(sendWindowDragEnd)();
+    });
+  const twoFingerTap = Gesture.Tap()
+    .maxDuration(200)
+    .minPointers(2)
+    .onStart((_event, success) => {
+      let state = {
+        finger: "right",
+        doubleTap: false,
+      };
+      runOnJS(sendClicks)(state);
+    });
+  const oneFingerTap = Gesture.Tap()
+    .maxDuration(200)
+    .onStart((_event, success) => {
+      let state = {
+        finger: "left",
+        doubleTap: false,
+      };
+      runOnJS(sendClicks)(state);
+    });
+  const oneFingerDoubleTap = Gesture.Tap()
+    .maxDuration(200)
+    .numberOfTaps(2)
+    .onStart((_event, success) => {
+      let state = {
+        finger: "left",
+        doubleTap: true,
+      };
+      runOnJS(sendClicks)(state);
+    });
+
+  const composed = Gesture.Race(
+    dragGestureWindowDrag,
+    dragGestureScroll,
+    dragGesture,
+    Gesture.Exclusive(twoFingerTap, oneFingerDoubleTap, oneFingerTap)
+  );
   return (
     <View style={styles.container}>
-      <PanGestureHandler onGestureEvent={touchPadGestureHandler}>
+      <GestureDetector gesture={composed}>
         <Animated.View style={styles.touchpad}></Animated.View>
-      </PanGestureHandler>
+      </GestureDetector>
 
       {/* <View style={styles.buttonContainer}>
         <TapGestureHandler
@@ -59,16 +124,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#1f1f1f",
     alignItems: "center",
     justifyContent: "center",
   },
   touchpad: {
     width: "100%",
-    height: 200,
+    height: 400,
     margin: 16,
     borderRadius: 8,
-    backgroundColor: "green",
+    backgroundColor: "white",
+    opacity: 0.1,
   },
   buttonContainer: {
     flexDirection: "row",
