277d0f60cd25128788bdbd6a068b709de30bae73
ayonshafiul <ayonshafiul@gmail.com>
2024-05-29 05:19:17 +0600

    feat: add ios splash and icons

--- react-native.config.js
+++ react-native.config.js
@@ -4,4 +4,11 @@ module.exports = {
     android: {},
   },
   assets: ['./assets/fonts'],
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
+  },
 };
