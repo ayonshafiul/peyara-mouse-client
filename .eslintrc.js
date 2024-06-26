77b982ab0235d2ab218003ac801b2bfa9af9c796
ayonshafiul <ayonshafiul@gmail.com>
2024-06-26 22:49:42 +0600

    chore: update prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
